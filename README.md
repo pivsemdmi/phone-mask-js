355a60dbd4eedaacb290038eafbe46d6485acd44
Semen Pivovarkin <pivsemdmi@gmail.com>
2023-09-16 00:54:58 +0600

    Refactoring

--- README.md
+++ README.md
@@ -5,11 +5,13 @@ Simple phone mask for Phone
 ## Include it
 
 After installing with npm, you could possibly do from index.html:
+
 ```html
 <script src="./node_modules/@pivsemdmi/phone-mask-js/phone-mask.js"></script>
 ```
 
 Use with webpack
+
 ```js
 const PhoneMask = require('@pivsemdmi/phone-mask-js');
 ```
@@ -17,18 +19,19 @@ const PhoneMask = require('@pivsemdmi/phone-mask-js');
 ## How to use
 
 ```js
+new PhoneMask('#phone');
 new PhoneMask('#phone', options);
 ```
 
 ### Options
 
-| Name      | Type | Default | Description                                                         |
-|-----------| ------------ |------------------|---------------------------------------------------------------------|
-| blurMask  | `boolean` | `true`              | Hide mask when empty value and blur                                 |
-| trimMask  | `boolean` | `false`             | Triming mask. See demo `minimal`                                    |
-| mask      | `string` | `+7 (___) ___-__-__` | Em-m... This is mask =D                                             |
-| softCaret | `string` | `_`                  | Caret from mask option                                              |
-| caret     | `string` | `_`                  | Caret show for user. Support two and more char. Not support numbers |
+| Name      | Type      | Default              | Description                                                         |
+|-----------|-----------|----------------------|---------------------------------------------------------------------|
+| blurMask  | `boolean` | `true`               | Hide mask when empty value and blur                                 |
+| trimMask  | `boolean` | `false`              | Triming mask. See demo `Trimming`                                   |
+| mask      | `string`  | `+7 (___) ___-__-__` | Em-m... This is mask =D                                             |
+| softCaret | `string`  | `_`                  | Caret from mask option                                              |
+| caret     | `string`  | `_`                  | Caret show for user. Support two and more char. Not support numbers |
 
 ## Detail work
 
--- phone-mask.js
+++ phone-mask.js
@@ -1,111 +1,118 @@
 /*
-* @pivsemdmi/phone-mask-js | v1.2.0
+* @pivsemdmi/phone-mask-js | v1.2.1
 * by Semen Pivovarkin.
 */
-class PhoneMask {
+
+class PhoneMaskOptions {
+    trimMask = false
+    blurMask = true
+    mask = '+7 (___) ___-__-__'
+    softCaret = '_'
+    caret = '_'
+}
+
+/**
+ * @extends {PhoneMaskOptions}
+ */
+class _PhoneMaskMagicOptions {
     /**
-     * @type {{
-     *   selector: string,
-     *   blurMask: boolean,
-     *   trimMask: boolean,
-     *   mask: string,
-     *   softCaret: string,
-     *   caret: string,
-     *   maskMinLength: int,
-     *   unmaskMaxLength: int,
-     * }}
+     * @type {PhoneMaskOptions}
      * @private
      */
-    _options = {
-        selector: undefined,
-        trimMask: false,
-        blurMask: true,
-        mask: '+7 (___) ___-__-__',
-        softCaret: '_',
-        caret: '_',
-        get maskMinLength() {
-            return Array.from(this.mask.matchAll(new RegExp(this.softCaret, 'g')))[0].index
-        },
-        get unmaskMaxLength() {
-            return this.mask.match(new RegExp(this.softCaret, 'g')).length
-        },
-    }
-    /**
-     * @type {array}
-     * @private
-     */
-    _fillingOptions = [
-        'selector',
-        'trimMask',
-        'blurMask',
-        'mask',
-        'softCaret',
-        'caret',
-    ]
-    /**
-     * @type {object}
-     * @private
-     */
-    _cache = {}
+    static _baseOptions = new PhoneMaskOptions();
 
     /**
-     * @return {HTMLInputElement|Element}
-     * @private
+     * @return {number}
      */
-    get _el() {
-        return this._cache.el
-            || (this._cache.el = document.querySelector(this._options.selector));
+    get maskMinLength() {
+        return Array.from(this.mask.matchAll(new RegExp(this.softCaret, 'g')))[0].index
     }
 
     /**
-     * @return {HTMLInputElement|Element}
+     * @return {number}
      */
-    get input() {
-        return this._el;
+    get unmaskMaxLength() {
+        return this.mask.match(new RegExp(this.softCaret, 'g')).length
     }
 
     /**
-     * @param {string} selector
-     * @param {{
-     *   blurMask: boolean,
-     *   trimMask: boolean,
-     *   mask: string,
-     *   softCaret: string,
-     *   caret: string,
-     * }} options
+     * @param {PhoneMaskOptions|{}} options
      */
-    constructor(selector, options = {}) {
-        this._initOptions(Object.assign(options, {selector}));
-        this._initInput();
+    constructor(options = {}) {
+        this._options = _PhoneMaskMagicOptions._initOptions(options);
 
-        this.update();
+        this._initOptionAccess();
+    }
+
+    /**
+     * @private
+     */
+    _initOptionAccess() {
+        _PhoneMaskMagicOptions._fillingOptions.forEach(optionName => {
+            Object.defineProperty(this, optionName, {
+                get: () => this._options[optionName],
+            });
+        });
+    }
+
+    /**
+     * @return {array}
+     * @private
+     */
+    static get _fillingOptions() {
+        return Object.keys(_PhoneMaskMagicOptions._baseOptions);
     }
 
     /**
      * @param {object} options
+     * @return {object} merged options
      * @private
      */
-    _initOptions(options) {
+    static _initOptions(options) {
+        return _PhoneMaskMagicOptions._fillOptions(options)
+    }
+
+    /**
+     * @param {object} options
+     * @return {object} merged options
+     * @private
+     */
+    static _fillOptions(options) {
         const
             interestOptionsEntries = Object.entries(options)
-                .filter(([key]) => this._fillingOptions.includes(key)),
+                .filter(([key]) => _PhoneMaskMagicOptions._fillingOptions.includes(key)),
             interestOptions = Object.fromEntries(interestOptionsEntries);
 
-        this._options = Object.assign(this._options, interestOptions);
+        const newOptions = Object.assign({},
+            _PhoneMaskMagicOptions._baseOptions,
+            interestOptions
+        );
+
+        _PhoneMaskMagicOptions._validate(newOptions);
+
+        return newOptions;
+    }
+
+    /**
+     * @param {object} options
+     * @throws {Error}
+     * @private
+     */
+    static _validate(options) {
 
         // Validate soft caret
-        this._checkNumberCaret(this._options.softCaret);
-        this._checkOneCharCaret(this._options.softCaret);
-        this._checkRegexCaret(this._options.softCaret);
+        this._checkNumberCaret(options.softCaret);
+        this._checkOneCharCaret(options.softCaret);
+        this._checkRegexCaret(options.softCaret);
 
         // Validate user caret
-        this._checkNumberCaret(this._options.caret);
+        this._checkNumberCaret(options.caret);
 
         // Validate phone mask
-        if ((new RegExp(`${this._options.softCaret}.*\\d`)).test(this._options.mask)) {
+        if ((new RegExp(`${options.softCaret}.*\\d`)).test(options.mask)) {
             throw new Error('Mask not support numbers after carets');
         }
-        if (!(new RegExp(`${this._options.softCaret}`)).test(this._options.mask)) {
+        if (!(new RegExp(`${options.softCaret}`)).test(options.mask)) {
             throw new Error('Soft caret not found in mask');
         }
     }
@@ -114,7 +121,7 @@ class PhoneMask {
      * @param {string} caret
      * @private
      */
-    _checkNumberCaret(caret) {
+    static _checkNumberCaret(caret) {
         if (/\d/.test(caret)) {
             throw new Error('Caret not support number format');
         }
@@ -124,7 +131,7 @@ class PhoneMask {
      * @param {string} caret
      * @private
      */
-    _checkOneCharCaret(caret) {
+    static _checkOneCharCaret(caret) {
         if (caret.length !== 1) {
             throw new Error('Caret support only one symbol');
         }
@@ -134,12 +141,60 @@ class PhoneMask {
      * @param {string} caret
      * @private
      */
-    _checkRegexCaret(caret) {
+    static _checkRegexCaret(caret) {
         try {
             (new RegExp(`${caret}`)).test(caret);
         } catch (e) {
             throw new Error(`Not supported caret "${caret}" in regex, please change`);
         }
+    }
+}
+
+class PhoneMask {
+    /**
+     * @extends {_PhoneMaskMagicOptions}
+     */
+    static Options = _PhoneMaskMagicOptions;
+
+    /**
+     * @type {PhoneMask.Options}
+     */
+    options;
+    /**
+     * @type {HTMLInputElement|Element}
+     * @private
+     */
+    _el;
+
+    /**
+     * @return {HTMLInputElement|Element}
+     */
+    get input() {
+        return this._el;
+    }
+
+    /**
+     * @param {HTMLInputElement|string} selector
+     * @param {PhoneMaskOptions|{}} options
+     */
+    constructor(selector, options = {}) {
+        this._init(selector);
+
+        this.options = new PhoneMask.Options(options);
+
+        this.update();
+    }
+
+    /**
+     * @private
+     */
+    _init(selector) {
+        this._el = selector instanceof HTMLInputElement
+            ? selector : document.querySelector(selector);
+
+        this._el.type = 'tel';
+
+        this._bind();
     }
 
     /**
@@ -161,8 +216,8 @@ class PhoneMask {
      * Updating blur
      */
     updateBlur() {
-        if (document.activeElement !== this._el) {
-            if (this._options.blurMask && !this.unmask) {
+        if (this.isBlur()) {
+            if (this.options.blurMask && !this.unmask) {
                 this._el.value = '';
             }
         } else {
@@ -171,11 +226,23 @@ class PhoneMask {
     }
 
     /**
+     * @return {boolean}
+     */
+    isFocus() {
+        return document.activeElement === this._el;
+    }
+
+    /**
+     * @return {boolean}
+     */
+    isBlur() {
+        return !this.isFocus();
+    }
+
+    /**
      * @private
      */
-    _initInput() {
-        this._el.type = 'tel';
-
+    _bind() {
         this._el.addEventListener('focus', this._onFocus);
         this._el.addEventListener('blur', this._onBlur);
         this._el.addEventListener('input', this._onInput);
@@ -208,28 +275,30 @@ class PhoneMask {
      * @private
      */
     _trimUnmask(unmask) {
-        return unmask.length <= this._options.unmaskMaxLength ? unmask
-            : unmask.replace(/^[87]/, '').slice(0, this._options.unmaskMaxLength);
+        return unmask.length <= this.options.unmaskMaxLength ? unmask
+            : unmask.replace(/^[87]/, '')
+                .slice(0, this.options.unmaskMaxLength);
     }
 
     /**
-     * @return {int}
+     * @return {number}
      * @private
      */
     get _unmaskPos() {
-        const maskPos = this._el.selectionEnd;
-        return this._el.value.slice(0, maskPos).replace(/\D/g, '').length - 1;
+        return this._el.value.slice(0, this._el.selectionEnd)
+            .replace(/\D/g, '').length - 1;
     }
 
     /**
-     * @param {int} unmaskPos
+     * @param {number} unmaskPos
      * @private
      */
     set _unmaskPos(unmaskPos) {
-        const minMaskPos = this._options.maskMinLength;
-        const maskPosMap = Array.from(this._el.value.matchAll(/\d/g))
-            .map(({index}) => index);
-        const maskPosition = maskPosMap[Math.min(unmaskPos, maskPosMap.length - 1)] + 1 || 0;
+        const
+            minMaskPos = this.options.maskMinLength,
+            maskPosMap = Array.from(this._el.value.matchAll(/\d/g))
+            .map(({index}) => index),
+            maskPosition = maskPosMap[Math.min(unmaskPos, maskPosMap.length - 1)] + 1 || 0;
 
         this._el.selectionStart = this._el.selectionEnd = Math.max(minMaskPos, maskPosition);
     }
@@ -238,14 +307,14 @@ class PhoneMask {
      * @return {string} value without mask
      */
     get unmask() {
-        const value = this._el.value;
-        const clearPatternSearch = '^' + this._options.mask
-                .replace(new RegExp(`([^${this._options.softCaret}\\d])`, 'g'), '')
+        const
+            value = this._el.value,
+            clearPatternSearch = '^' + this.options.mask
+                .replace(new RegExp(`([^${this.options.softCaret}\\d])`, 'g'), '')
                 .replace(/(\d)/g, '$1?')
-                .replace(new RegExp(`${this._options.softCaret}+`, 'g'), '(\\d*?)') + '$',
-            clearPattern = new RegExp(clearPatternSearch);
-
-        const matches = clearPattern.exec(value.replace(/\D/g, ''));
+                .replace(new RegExp(`${this.options.softCaret}+`, 'g'), '(\\d*?)') + '$',
+            clearPattern = new RegExp(clearPatternSearch),
+            matches = clearPattern.exec(value.replace(/\D/g, ''));
 
         return matches && matches.slice(1)
             .map(val => val.replace(/\D/g, ''))
@@ -256,25 +325,26 @@ class PhoneMask {
      * @param {string} unmask value without mask
      */
     set unmask(unmask) {
-        const patternSearch = unmask.replace(/\d/g, '(\\d)'),
-            pattern = new RegExp(patternSearch);
+        const
+            patternSearch = unmask.replace(/\d/g, '(\\d)'),
+            pattern = new RegExp(patternSearch),
+            count = Math.min(unmask.length, this.options.unmaskMaxLength);
 
-        const count = Math.min(unmask.length, this._options.unmaskMaxLength);
-
-        let replaceValue = this._options.mask;
+        let replaceValue = this.options.mask;
 
         for (let i = 1; i <= count; i++) {
-            replaceValue = replaceValue.replace(new RegExp(this._options.softCaret), '$' + i)
+            replaceValue = replaceValue.replace(new RegExp(this.options.softCaret), '$' + i)
         }
 
         let maskedValue = unmask.replace(pattern, replaceValue);
 
-        if (this._options.trimMask) {
-            const minMaskLen = this._options.maskMinLength;
-            const lastNumber = Array.from(maskedValue.matchAll(/\d/g)).reverse()[0].index + 1;
+        if (this.options.trimMask) {
+            const
+                minMaskLen = this.options.maskMinLength,
+                lastNumber = Array.from(maskedValue.matchAll(/\d/g)).reverse()[0].index + 1;
             maskedValue = maskedValue.slice(0, Math.max(minMaskLen, lastNumber));
-        } else if (this._options.softCaret !== this._options.caret) {
-            maskedValue = maskedValue.replace(new RegExp(this._options.softCaret, 'g'), this._options.caret);
+        } else if (this.options.softCaret !== this.options.caret) {
+            maskedValue = maskedValue.replace(new RegExp(this.options.softCaret, 'g'), this.options.caret);
         }
 
         this._el.value = maskedValue;
